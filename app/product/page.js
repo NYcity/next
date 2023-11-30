f528d27ef99527a92d12a637bf58afe9556e005d
Hongki Choi <mossei75@gmail.com>
2023-11-30 11:24:39 +0800

    s

---
+++ app/product/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const product = () => {
+    return (
+        <>
+            <div>product Page</div>
+            <Link href='/'>Go Index</Link>
+        </>
+    )
+}
+
+export default product
